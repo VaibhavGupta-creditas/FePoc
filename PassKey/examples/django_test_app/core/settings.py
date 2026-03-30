b9ae21fe8ea6db7fba2bfc421717063dbed9bc60
Vaibhav Gupta <101336837+VaibhavGupta-creditas@users.noreply.github.com>
2026-03-30 12:23:50 +0530

    update redis url

--- PassKey/examples/django_test_app/core/settings.py
+++ PassKey/examples/django_test_app/core/settings.py
@@ -73,7 +73,7 @@ TEMPLATES = [
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": os.environ.get("REDIS_URL", "redis://127.0.0.1:6379/1"),
+        "LOCATION": os.environ.get("REDIS2_URL", "redis://127.0.0.1:6379/1"),
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
